28edd41ed0bba4ab70e2f612fe69da3a8df5ab5c
pakuyuya <pakuyuya@yahoo.co.jp>
2019-06-22 12:44:14 +0900

    fix code

--- pyarrow/sample.py
+++ pyarrow/sample.py
@@ -2,6 +2,6 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-df = pd.read_csv('.\\input\\test.csv')
+df = pd.read_csv('input/test.csv')
 table = pa.Table.from_pandas(df)
-pd.write_table(table, './outout/test.parquet')
+pq.write_table(table, 'output/test.parquet')
